95910228f06064791baf112eabc9b2bc6adb90c9
bruholanda <brunaholandacunha7@gmail.com>
2022-03-07 17:48:21 -0300

    Requirement 8;

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -10,6 +10,7 @@ const Login = () => {
     localStorage.setItem('mealsToken', 1);
     localStorage.setItem('cocktailsToken', 1);
     localStorage.setItem('user', JSON.stringify(emailStorage));
+    window.location.href = '/foods';
   };
 
   const validadeButton = () => {
