17062a0589e4b5d7a5286935b4542c58223b6151
Felipe Castanheira Carvalho <castanheirafc2@gmail.com>
2022-03-08 15:39:16 -0300

    req 26

--- src/components/Drinks.jsx
+++ src/components/Drinks.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Footer from './Footer';
 import Header from './Header';
+import Recipes from './Recipes';
 
 function Drinks() {
   return (
     <div>
       <Header hasProfileIcon hasSearchIcon name="Drinks" />
+      <Recipes isDrink />
       <Footer />
     </div>
   );
--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -1,21 +1,32 @@
 import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
 import AppContext from '../context/AppContext';
 import { MAX_RECIPES } from '../services/consts';
 
-function Recipes() {
+function Recipes({ isDrink }) {
   const { data } = useContext(AppContext);
   return (
     data.filter((_meal, index) => index < MAX_RECIPES)
-      .map((_meal, index) => (
+      .map((meal, index) => (
         <div
           data-testid={ `${index}-recipe-card` }
           key={ `${index}-recipe-card` }
         >
-          <div data-testid={ `${index}-card-img` }>b</div>
-          <div data-testid={ `${index}-card-name` }>c</div>
+          <img
+            data-testid={ `${index}-card-img` }
+            alt="recipe"
+            src={ isDrink ? meal.strDrinkThumb : meal.strMealThumb }
+          />
+          <h4 data-testid={ `${index}-card-name` }>
+            { isDrink ? meal.strDrink : meal.strMeal }
+          </h4>
         </div>
       ))
   );
 }
+
+Recipes.propTypes = {
+  isDrink: PropTypes.bool.isRequired,
+};
 
 export default Recipes;
