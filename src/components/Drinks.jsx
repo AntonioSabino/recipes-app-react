0a7083a60791294b79c02530ac134a2edd80d600
Victor Roesner <victorroesner@hotmail.com>
2022-03-08 14:53:15 -0300

    req 24

--- src/components/Drinks.jsx
+++ src/components/Drinks.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Footer from './Footer';
 import Header from './Header';
 
 function Drinks() {
   return (
     <div>
       <Header hasProfileIcon hasSearchIcon name="Drinks" />
+      <Footer />
     </div>
   );
 }
--- src/components/ExploreFoods.jsx
+++ src/components/ExploreFoods.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Footer from './Footer';
 import Header from './Header';
 
 function ExploreFoods() {
   return (
     <div>
       <Header hasProfileIcon name="Explore Foods" />
+      <Footer />
     </div>
   );
 }
