56cb5eecacb69b75196f220b172f81d6ca8cf762
Victor Roesner <victorroesner@hotmail.com>
2022-03-08 14:43:04 -0300

    req 20

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,29 +1,32 @@
 import React from 'react';
+import drinkIcon from '../images/drinkIcon.svg';
+import exploreIcon from '../images/exploreIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
 
 function Footer() {
   return (
     <footer
+      className="footer"
       data-testid="footer"
     >
       <button
         type="button"
-        data-testid="drinks-bottom-btn"
       >
-        drink
+        <img src={ drinkIcon } alt="Drink icon" data-testid="drinks-bottom-btn" />
 
       </button>
       <button
         type="button"
-        data-testid="explore-bottom-btn"
+
       >
-        explore
+        <img src={ exploreIcon } alt="explore icon" data-testid="explore-bottom-btn" />
 
       </button>
       <button
         type="button"
-        data-testid="food-bottom-btn"
+
       >
-        food
+        <img src={ mealIcon } alt="Meal icon" data-testid="food-bottom-btn" />
 
       </button>
     </footer>
