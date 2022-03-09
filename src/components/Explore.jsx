fc230deafc4b9b496b8eaba844dbc90e58d88eb6
Felipe Castanheira <castanheirafc2@gmail.com>
2022-03-09 19:33:54 -0300

    :sparkles: feat: explore page

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from './Footer';
 import Header from './Header';
 
@@ -6,6 +7,14 @@ function Explore() {
   return (
     <div>
       <Header hasProfileIcon name="Explore" />
+      <section className="explore-container">
+        <Link data-testid="explore-foods" to="/explore/foods">
+          <h5>Explore Foods</h5>
+        </Link>
+        <Link data-testid="explore-drinks" to="/explore/drinks">
+          <h5>Explore Drinks</h5>
+        </Link>
+      </section>
       <Footer />
     </div>
   );
