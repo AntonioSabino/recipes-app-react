e8a73b1aa2283dd3e18f7d298cf93d50ac33a314
Felipe Castanheira <castanheirafc2@gmail.com>
2022-03-09 23:14:07 -0300

    :sparkles: feat: ingredients page

--- src/services/consts.js
+++ src/services/consts.js
@@ -3,3 +3,7 @@ export const FST_LETTER = 'First Letter';
 export const MAX_RECIPES = 12;
 
 export const MAX_TYPES = 5;
+
+export const MEAL_ING_URL = 'https://www.themealdb.com/images/ingredients/';
+
+export const DRINK_ING_URL = 'https://www.thecocktaildb.com/images/ingredients/';
