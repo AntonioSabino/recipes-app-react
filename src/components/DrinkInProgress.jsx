a8ef173a697cbfb4bcc3b9eafe838347449d6313
Antonio Sabino <aslimajr@outlook.com>
2022-03-10 22:08:54 -0300

    Resoluçao requisito 47

--- src/components/DrinkInProgress.jsx
+++ src/components/DrinkInProgress.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import DrinkDetails from './DrinkDetails';
 
-function DrinkInProgress() {
+function DrinkInProgress({ match }) {
   return (
     <div>
-      drinkinprogress
+      <DrinkDetails match={ match } />
+      <button type="button" data-testid="finish-recipe-btn">
+        Finalizar
+      </button>
     </div>
   );
 }
+
+DrinkInProgress.propTypes = {
+  match: PropTypes.string,
+}.isRequired;
 
 export default DrinkInProgress;
