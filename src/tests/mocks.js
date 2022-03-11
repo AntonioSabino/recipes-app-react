a3b577f51b634c9d440b0649384886a23f13fe61
Felipe Castanheira <castanheirafc2@gmail.com>
2022-03-11 17:52:01 -0300

    test: mocks

---
+++ src/tests/mocks.js
@@ -0,0 +1,16 @@
+export const FOOD_BTNS = ['All', 'Beef', 'Breakfast', 'Chicken', 'Dessert', 'Goat'];
+
+export const NUMB_OF_RECIPES = 12;
+
+export const EXPLORE_IMGS = ['profile', 'drinks', 'explore', 'foods'];
+
+export const EXPLORE_FOODS_LINKS = ['By Ingredient', 'By Nationality', 'Surprise me!'];
+
+export const H5_LEVEL = 5;
+
+export const PATH = {
+  exploreFoods: '/explore/foods',
+  ingredients: '/explore/foods/ingredients',
+  nationalities: '/explore/foods/nationalities',
+  details: '/foods/52977',
+};
