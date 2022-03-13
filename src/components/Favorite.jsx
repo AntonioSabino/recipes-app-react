57cdfd6d175e1a0bd76bb0de8771dc3de20eb0e3
Felipe Castanheira <castanheirafc2@gmail.com>
2022-03-13 12:59:40 -0300

    :sparkles: feat: favoriteRecipes page

--- src/components/Favorite.jsx
+++ src/components/Favorite.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import DoneFilters from './DoneFilters';
+import FavCards from './FavCards';
 import Header from './Header';
 
 function Favorite() {
   return (
     <div>
       <Header hasProfileIcon name="Favorite Recipes" />
+      <DoneFilters />
+      <FavCards />
     </div>
   );
 }
