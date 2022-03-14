653aad344e19c468fdaf3ae304c1cfd5f3a4a4aa
Felipe Castanheira Carvalho <castanheirafc2@gmail.com>
2022-03-14 18:21:33 -0300

    style: explore btns

--- src/components/ExploreButtons.jsx
+++ src/components/ExploreButtons.jsx
@@ -13,17 +13,22 @@ function ExploreButtons({ isDrink }) {
   return (
     <nav className="explore-nav">
       <Link
+        className="explore-btn"
         data-testid="explore-by-ingredient"
         to={ isDrink ? '/explore/drinks/ingredients' : '/explore/foods/ingredients' }
       >
         <h5>By Ingredient</h5>
       </Link>
       { !isDrink && (
-        <Link data-testid="explore-by-nationality" to="/explore/foods/nationalities">
+        <Link
+          className="explore-btn"
+          data-testid="explore-by-nationality"
+          to="/explore/foods/nationalities"
+        >
           <h5>By Nationality</h5>
         </Link>
       )}
-      <button type="button" onClick={ handleClick }>
+      <button className="explore-btn" type="button" onClick={ handleClick }>
         <h5 data-testid="explore-surprise">Surprise me!</h5>
       </button>
     </nav>
