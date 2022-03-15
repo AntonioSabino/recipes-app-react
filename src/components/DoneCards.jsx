515f0b5e9a3b12346bedc90d47f126ab8b13cc04
Felipe Castanheira Carvalho <castanheirafc2@gmail.com>
2022-03-15 15:33:23 -0300

    :bug: fix: useContext (import)

--- src/components/DoneCards.jsx
+++ src/components/DoneCards.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import AppContext from '../context/AppContext';
 import shareIcon from '../images/shareIcon.svg';
 
 function DoneCards() {
